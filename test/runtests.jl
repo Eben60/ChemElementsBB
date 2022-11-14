3e48cdf44baaf573c15c317e28ed940bbf61779b
Ben Elkin <8xe8n-cm@online.de>
2022-11-14 23:01:24 +0100

    some tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,4 +2,11 @@ using ChemElementsBB
 
 using Test
 
+@import_els K Ca P O
 
+@test K / (K + Ca + P + 4*O) ≈ 0.22451440791379498
+
+@test K === chem_els["potassium"]
+@test Ca === chem_els[:Ca]
+@test P === chem_els[15] 
+@test O === chem_els.O
